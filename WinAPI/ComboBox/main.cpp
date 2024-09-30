01e085f5cb7a1f946ae68e84c5566174d76dc0d6
Viktor <Viktor@VOKTOR-PC1>
2024-09-30 21:41:18 +0300

    done

---
+++ WinAPI/ComboBox/main.cpp
@@ -0,0 +1,39 @@
+#include <Windows.h>
+#include "resource.h"
+
+CONST CHAR* g_COMBO_BOX_ITEMS[] = { "This","is","my","First","Combo","Box" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInt, LPSTR ipCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+		for (int i = 0; i < sizeof(g_COMBO_BOX_ITEMS)/sizeof(g_COMBO_BOX_ITEMS[0]); i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_COMBO_BOX_ITEMS[i]);
+			//CB_ - ComboBox
+		}
+		// CB_SETCORSEL - ComboBox Set Current Selection
+		SendMessage(hCombo, CB_SETCURSEL, 0, 0);
+	}
+	break;
+	case WM_COMMAND:
+
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
