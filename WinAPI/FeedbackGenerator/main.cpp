48cd0a48b222525c09a1ba79f8788efee1790c96
Viktor <Viktor@VOKTOR-PC1>
2024-10-09 18:30:44 +0300

    FeedbackGenerator Done

---
+++ WinAPI/FeedbackGenerator/main.cpp
@@ -0,0 +1,72 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <stdio.h>
+#include "resource.h"
+
+CONST CHAR* SZ_PROPERTIES[] =
+{
+    "понимает материал",
+    "внимательный",
+    "хорошо пишет код",
+    "не выполняет домашние задания",
+    "прогуливает занятия"
+};
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+    return 0;
+}
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        SendMessage(GetDlgItem(hwnd, IDC_EDIT_NAME), WM_SETTEXT, 0, (LPARAM)"Введите Фамилию Имя Отчество поностью");
+    }
+    break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+        {
+            CONST INT SIZE_DESCR = 2048, SIZE = 256;
+            CHAR sz_fullname[SIZE]{};
+            CHAR sz_description[SIZE_DESCR]{ "Введите Фамилию Имя Отчество поностью" };
+            HWND hEditName = GetDlgItem(hwnd, IDC_EDIT_NAME);
+            HWND hEditDescription = GetDlgItem(hwnd, IDC_EDIT_DESCRIPTION);
+            SendMessage(hEditName, WM_GETTEXT, SIZE, (LPARAM)sz_fullname);
+            if (strcmp(sz_description, sz_fullname) == 0 || strcmp(sz_fullname, "") == 0)
+            {
+                SendMessage(hEditDescription, WM_SETTEXT, 0, (LPARAM)"Вы не ввели Ф.И.О студента");
+            }
+            else
+            {
+                
+                strcpy(sz_description, sz_fullname);
+                sprintf(sz_fullname, "Характеристики студента: %s", sz_description);
+                SendMessage(GetDlgItem(hwnd, IDC_STATIC_CHARACTER), WM_SETTEXT, 0, (LPARAM)sz_fullname);
+                for (int i = 0; i < 5; i++)
+                {
+                    HWND hCheck = GetDlgItem(hwnd, IDC_CHECK_UNDERSTANDING + i);
+                    if (SendMessage(hCheck, BM_GETCHECK, 0, 0) == BST_CHECKED)
+                        sprintf(sz_description, "%s, %s", sz_description, SZ_PROPERTIES[i]);
+                }
+
+                SendMessage(hEditDescription, WM_SETTEXT, 0, (LPARAM)sz_description);
+            }
+        }
+        break;
+        case IDCANCEL:
+            EndDialog(hwnd, 0);
+            break;
+        }
+        break;
+    case WM_CLOSE:
+        EndDialog(hwnd, 0);
+    }
+    return FALSE;
+}
---
+++ WinAPI/FeedbackGenerator/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_NAME                        1001
+#define IDC_EDIT2                       1004
+#define IDC_EDIT_DESCRIPTION            1004
+#define IDC_STATIC_ADVANTAGES           1005
+#define IDC_STATIC_DISADVANTAGES        1006
+#define IDC_EDIT_NAME                   1007
+#define IDC_CHECK_UNDERSTANDING         1009
+#define IDC_CHECK_ATTENTION             1010
+#define IDC_CHECK_GOOD_CODING           1011
+#define IDC_CHECK_NO_HOMEWORKS          1012
+#define IDC_CHECK_BAD_ATTENDANCE        1013
+#define IDC_STATIC_CHARACTER            1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
